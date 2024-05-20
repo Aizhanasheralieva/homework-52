c4e19aa5eb2126892b2d69037b6bc1925ae53d3f
Sheralieva Aizhana <asheralieva7@gmail.com>
2024-05-21 01:14:06 +0600

    Level 3 is completed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import './App.css';
 import CardDeck from "./lib/CardDeck.ts";
+import {useState} from "react";
 
-
-
+interface Props {
+    rank: string;
+    suit: string;
+}
 
 let cardDeck = new CardDeck();
 
@@ -15,11 +18,27 @@ multipleCards.forEach(card => {
     console.log(`${card.rank} ${card.suit}`);
 });
 const App = () => {
+    const [cards, setCards] = useState([]);
 
+    const dealCards = (): void => {
+        const deck = new CardDeck();
+        const dealtCards = deck.getCards(5);
+        setCards(dealtCards);
+    };
 
     return (
-        <div>
-
+        <div className="App">
+            <button className="dealCardsBtn" onClick={dealCards}>Раздать карты</button>
+            {cards.length > 0 && (
+                <div className="playingCards faceImages">
+                    {cards.map((card, index) => (
+                        <span key={index} className={`card rank-${card.rank.toLowerCase()} ${card.suit}`}>
+              <span className="rank">{card.rank}</span>
+              <span className="suit">{card.suit === 'diams' ? '♦' : card.suit === 'hearts' ? '♥' : card.suit === 'clubs' ? '♣' : '♠'}</span>
+            </span>
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
